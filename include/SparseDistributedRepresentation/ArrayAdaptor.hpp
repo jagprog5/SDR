ba3435e440db843437c2a645ce6ad020a1b9e6f6
john <johngiorshev@gmail.com>
2022-07-07 16:05:17 -0400

    improve ArrayAdaptor

--- include/SparseDistributedRepresentation/ArrayAdaptor.hpp
+++ include/SparseDistributedRepresentation/ArrayAdaptor.hpp
@@ -5,13 +5,18 @@
  * An ArrayAdaptor provides a vector-like interface so an array can be used in an SDR.
  * This is needed for statically allocated SDRs.
  * 
- * No bound checking is done. Ensure that the size if sufficient. e.g. or elements can produce an output with a size only up to (inclusively) the sum of the arguments' sizes
+ * Ensure that the size if sufficient. e.g. or elements can produce an output with a size only up to (inclusively) the sum of the arguments' sizes
  */
 template<typename SDRElem_t, std::size_t N>
 class ArrayAdaptor {
     private:
         std::array<SDRElem_t, N> arr_;
         SDRElem_t* end_;
+
+    void drop_content(SDRElem_t* pos) {
+        // moves from the element, then drops the contents
+        [[maybe_unused]] SDRElem_t r = std::move(*pos);
+    }
 
     public:
         using value_type = SDRElem_t;
@@ -72,34 +77,40 @@ class ArrayAdaptor {
         reverse_iterator rend() { return std::reverse_iterator(begin()); }
 
         const_reverse_iterator crbegin() const { return std::reverse_iterator(cend()); }
-
         const_reverse_iterator crend() const { return std::reverse_iterator(cbegin()); }
 
-        void push_back(const value_type& val) {
-            #ifndef NDEBUG
-                assert((size_t)(end_ - &arr_[0]) < N);
-            #endif
+        template<typename E>
+        void push_back(E&& val) {
+            assert(size() < N);
             // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
             *end_++ = val;
         }
 
         void erase(iterator pos) {
-            #ifndef NDEBUG
-                assert(!empty());
-            #endif
+            assert(!empty());
             while (++pos != end_) {
-                *(pos - 1) = *pos;
+                *(pos - 1) = std::move(*pos);
             }
             --end_;
+            drop_content(end_);
         }
 
         void resize(size_t size) {
-            assert(size <= N);
+            assert(size <= this->size() && "resize can only shrink an ArrayAdaptor");
             // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-            end_ = &arr_[0] + size;
+            SDRElem_t* new_end = &arr_[0] + size;
+            SDRElem_t* pos = new_end;
+            while (pos != end()) {
+                drop_content(pos++);
+            }
+            end_ = new_end;
         }
 
         void clear() {
+            SDRElem_t* pos = begin();
+            while (pos != end()) {
+                drop_content(pos++);
+            }
             end_ = begin();
         }
 
